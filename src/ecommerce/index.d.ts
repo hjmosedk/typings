754062a0ea42cff03b35bab68cb2376f25323eeb
Christian Kubel Højmose <christian@hjmose.dk>
2024-04-01 12:43:08 +0200

    fix(ecommer.index.d.ts): fixed an error in the file

--- src/ecommerce/index.d.ts
+++ src/ecommerce/index.d.ts
@@ -1,4 +1,4 @@
 import { ProductModel } from './productModel';
 import { CurrencyType } from './currencyModel';
 
-export default { ProductModel, CurrencyType };
+export { ProductModel, CurrencyType };
